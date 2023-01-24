04516f3fdf9ff10d7f49cd35ef371f8fa19a99e4
Alex <heikun@yandex.ru>
2023-01-24 22:52:32 +0400

    Lesson 16 - props

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,14 @@
 import s from './Post.module.css'
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/ice-mountain-mountains-snow-stars-background-design-template-a6af0eb69fdbbf26ec92f3dc506a7d17_screen.jpg?ts=1589741832" />
             <div className={s.content}>
-                <p>Post 1</p>
-                <date>18.02.2022</date>
+                <p>{props.message}</p>
+                <date>{props.date}</date>
                 <div>
+                    <span>{props.countLike}</span>
                     <button>Like</button>
                     <button>Dislike</button>
                 </div>
