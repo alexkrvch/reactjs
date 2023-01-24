9b59d0d99ac85d36a85bd3311b31f877c3a0f118
Alex <heikun@yandex.ru>
2023-01-25 00:49:21 +0400

    Lesson 19 install router and added routes for pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,11 +3,11 @@ import s from './Nav.module.css'
 const Nav = () => {
     return (
         <nav className={s.nav}>
-            <a href="rl/src/components#" className={`${s.item} ${s.active}`}><span>Profile</span></a>
-            <a href="rl/src/components#" className={s.item}><span>Message</span></a>
-            <a href="rl/src/components#" className={s.item}><span>Music</span></a>
-            <a href="rl/src/components#" className={s.item}><span>News</span></a>
-            <a href="rl/src/components#" className={s.item}><span>Settings</span></a>
+            <a href="/profile" className={`${s.item} ${s.active}`}><span>Profile</span></a>
+            <a href="/dialogs" className={s.item}><span>Message</span></a>
+            <a href="/music" className={s.item}><span>Music</span></a>
+            <a href="/news" className={s.item}><span>News</span></a>
+            <a href="/settings" className={s.item}><span>Settings</span></a>
         </nav>
     )
 }
