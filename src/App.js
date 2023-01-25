35e288a4262b8fd509747ddbe843a69291ecbbd9
Alex <heikun@yandex.ru>
2023-01-25 20:06:13 +0400

    Lesson-22 added active class for dialogs and changed style and added route

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Profile />} />
                     <Route path="/profile" element={<Profile />} />
-                    <Route path="/dialogs" element={<Dialogs />} />
+                    <Route path="/dialogs/*" element={<Dialogs />} />
                     <Route path="/music" element={<Music />} />
                     <Route path="/news" element={<News />} />
                     <Route path="/settings" element={<Settings />} />
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,5 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
@@ -6,31 +7,31 @@ const Dialogs = () => {
             <h1>Dialogs</h1>
             <div className={s.content_dialogs}>
                 <div className={s.dialogs_list}>
-                    <div className={`${s.dialogs_item} ${s.active}`}>
+                    <NavLink to="/dialogs/1" className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
                         <span>Alex</span>
                         <span>last message</span>
                         <span>28.10.2022</span>
-                    </div>
-                    <div className={s.dialogs_item}>
+                    </NavLink>
+                    <NavLink to="/dialogs/2" className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
                         <span>Andy</span>
                         <span>last message</span>
                         <span>19.09.2022</span>
-                    </div>
-                    <div className={s.dialogs_item}>
+                    </NavLink>
+                    <NavLink to="/dialogs/3" className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
                         <span>Mikhail</span>
                         <span>last message</span>
                         <span>12.04.2022</span>
-                    </div>
-                    <div className={s.dialogs_item}>
+                    </NavLink>
+                    <NavLink to="/dialogs/4" className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
                         <span>Ragnar</span>
                         <span>last message</span>
                         <span>20.05.2021</span>
-                    </div>
-                    <div className={s.dialogs_item}>
+                    </NavLink>
+                    <NavLink to="/dialogs/5" className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
                         <span>Eriks</span>
                         <span>last message</span>
                         <span>04.01.2021</span>
-                    </div>
+                    </NavLink>
                 </div>
                 <div className={s.messages_list}>
                     <div className={s.message_item}>
