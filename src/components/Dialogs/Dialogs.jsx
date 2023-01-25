fb8b03b009731dacc76e4242a479b542e044ddff
Alex <heikun@yandex.ru>
2023-01-26 00:07:40 +0400

    Lesson-26 create new components and clean dialogs.jsx

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,6 @@
 import s from './Dialogs.module.css'
-import {NavLink} from "react-router-dom";
-
-const Dialog = (props) => {
-    return (
-        <NavLink to={`/dialogs/${props.url}`} className={({ isActive }) => isActive ? `${s.dialogs_item} ${s.active}` : s.dialogs_item}>
-            <span>{props.name}</span>
-            <span>{props.lastMessage}</span>
-            <span>{props.dateMessage}</span>
-        </NavLink>
-    )
-}
-
-const Message = (props) => {
-    return (
-        <div className={s.message_item}>
-            <p>{props.text}</p>
-            <p>{props.dateMessage}</p>
-        </div>
-    )
-}
+import Dialog from "./Dialog/Dialog";
+import Message from "./Message/Message";
 
 const Dialogs = () => {
 
