2cd889ee53c694681cab2447c5cffdc1506e1cc5
Alex <heikun@yandex.ru>
2023-01-25 22:44:28 +0400

    Lesson-24 added object for post/dialog/message items

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,28 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+
+    let postData = [
+        {
+            id: 1,
+            message: "It's my first post",
+            date: "18.02.2022",
+            countLike: 29
+        },
+        {
+            id: 2,
+            message: "My demo post",
+            date: "24.02.2022",
+            countLike: 14
+        },
+        {
+            id: 3,
+            message: "Hello world",
+            date: "29.02.2022",
+            countLike: 1
+        },
+    ]
+
     return (
         <div className={s.myPosts}>
             <h3>My posts</h3>
