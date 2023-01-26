9a9da3bac540f9097aceaecc9477c20c1b24ea10
Alex <heikun@yandex.ru>
2023-01-27 01:20:11 +0400

    Lesson-33 change render type

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,7 +4,7 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-    let posts = props.postData.map(p => <Post message={p.message} date={p.date} countLike={p.countLike} id={p.id} key={p.id} />)
+    let posts = props.state.postData.map(p => <Post message={p.message} date={p.date} countLike={p.countLike} id={p.id} key={p.id} />)
 
     let newPostArea = React.createRef();
 
@@ -17,7 +17,7 @@ const MyPosts = (props) => {
         <div className={s.myPosts}>
             <h3>My posts</h3>
             <div className={s.newPost}>
-                <textarea ref={newPostArea}></textarea>
+                <textarea ref={newPostArea} value={props.state.textAreaText}></textarea>
                 <div className={s.buttons}>
                     <button onClick={ onAddPost }>Add post</button>
                     <button>Remove</button>
