4b4c95c864213a041e38125f4afac6614c262508
Alex <heikun@yandex.ru>
2023-01-27 15:15:11 +0400

    Lesson-35 delete render.js and add subscribe for rerender

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import reportWebVitals from './reportWebVitals';
-import state from './redux/state';
-import {rerenderEntireTree} from "./render";
+import state, {addPost, changePostText, subscribe} from './redux/state';
+import React from "react";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+import ReactDOM from "react-dom/client";
 
-rerenderEntireTree(state);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let rerenderEntireTree = (state) => {
+    root.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state} addPost={addPost} changePostText={changePostText}/>
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+}
+
+rerenderEntireTree(state)
+
+subscribe(rerenderEntireTree);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
