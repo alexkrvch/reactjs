ae69f91c28d5734e2f7ebb81e9617d608307bbeb
Alex <heikun@yandex.ru>
2023-01-29 22:58:06 +0400

    Lesson-42 installed redux and used

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className={s.header}>
             <a href="/" className={s.logo}>
-                <img src="/logo.png" />
+                <img src="/logo.png" alt="" />
             </a>
         </header>
     )
--- src/redux/navReducer.js
+++ src/redux/navReducer.js
@@ -1,4 +1,24 @@
-const navReducer = (state, action) => {
+const initialState = {
+    users:[
+        {
+            id: 1,
+            name: "Alex",
+            img: "https://image.shutterstock.com/image-vector/lorem-ipsum-dolor-tag-cloud-260nw-1103263931.jpg"
+        },
+        {
+            id: 2,
+            name: "Andy",
+            img: "https://rehabconceptspt.com/wp-content/uploads/2016/06/placeholder-640-square.jpg"
+        },
+        {
+            id: 3,
+            name: "Mikhail",
+            img: "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/ice-mountain-mountains-snow-stars-background-design-template-a6af0eb69fdbbf26ec92f3dc506a7d17_screen.jpg?ts=1589741832"
+        }
+    ]
+}
+
+const navReducer = (state = initialState, action) => {
 
     return state;
 }
